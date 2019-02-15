7672eccdb4984b193d57341fa7cab3ce3b9a48c4
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-02-15 14:14:25 +0900

    add source files
    The file locations are not fixed, you can change it in the webpack config.
    
    `main_window.tsx` is the main widget for the demo.
    `index.tsx` will attach the widget to `index.html`.
    
    We are not ready to build to code yet.

---
+++ src/ts/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { MainWindow } from './main_window';
+
+
+ReactDOM.render((
+  <MainWindow />
+), document.querySelector('#main-window'));
