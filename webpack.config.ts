5df00f9d99b7ba039081f77a94913d65c70ddd24
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-04-01 16:06:42 +0900

    let webpack transpile tsx
    Now we can actually write the rule for TypeScript.
    This will be a long journey.
    
    The transpile pipeline works like this:
    TSX|TS -> JSX|JS
    
    TypeScript itself can transpile TS or TSX, even JSX.
    But unfortunately the ecosystem still highly depends on Babel, so we need to
    find a way to adopt Babel into TypeScript pipeline.
    
    So we will use TypeScript to transpile TS and TSX files, then use Babel to
    transpile JSX and JS files.
    
    First we need Babel core.
    
    ```
    yarn add -D @babel/core
    ```
    
    Then we need presets to compile JSX.
    `@babel/preset-env` will automatically enable needed new features in
    ECMAScript.
    `@babel/preset-react` contains needed features for JSX.
    
    ```
    yarn add -D @babel/preset-env @babel/preset-react
    ```
    
    We need `babel-loader` to let Webpack use Babel to compile JS and JSX files.
    
    ```
    yarn add -D babel-loader
    ```
    
    We use `awesome-typescript-loader` to load TypeScript for Webpack.
    It seems `awesome-typescript-loader` has better support to Babel compared to
    `ts-loader`.
    
    ```
    yarn add -D awesome-typescript-loader
    ```
    
    The steps above just make the script uses React, so we still need to install
    React itself.
    We also need `react-dom` for DOM operations.
    
    ```
    yarn add react react-dom
    ```
    
    Do not forget to install type information for TypeScript.
    
    ```
    yarn add -D @types/react @types/react-dom
    ```

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,6 +2,37 @@ import webpack from 'webpack';
 
 
 const config: webpack.Configuration = {
+  entry: './src/ts/index.tsx',
+  module: {
+    rules: [
+      // js
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+           loader: 'babel-loader',
+          },
+        ],
+      },
+      // ts, tsx
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+           loader: 'babel-loader',
+          },
+          {
+           loader: 'awesome-typescript-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
 };
 
 
