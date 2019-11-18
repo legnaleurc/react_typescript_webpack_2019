157f13eabb2bacc69e632dd4098962ee570cbeb5
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-11-18 10:35:11 +0900

    let webpack transpile tsx
    Now we can actually write the rule for TypeScript.
    
    The transpile pipeline works like this:
    TSX|TS -> JSX|JS
    
    TypeScript itself can transpile TS or TSX, even JSX.
    But unfortunately the ecosystem still highly depends on Babel, so we need to
    find a way to adopt Babel into TypeScript pipeline.
    
    But a good news is Babel will handle the pipeline for you, so we just need to
    plug needed parts to the configuration.
    
    For this we need `babel-loader` to let Webpack load Babel to compile
    JS|JSX|TS|TSX files.
    
    ```
    yarn add -D babel-loader
    ```
    
    And Babel need `@babel/preset-react` to know how to compile React syntax.
    Do not forget to add this preset to `.babelrc`.
    
    ```
    yarn add -D @babel/preset-react
    ```
    
    The steps above just make the script uses React, so we still need to install
    React itself.
    We also need `react-dom` for DOM operations.
    
    ```
    yarn add react react-dom
    ```
    
    Do not forget to install type information for TypeScript.
    
    ```
    yarn add -D @types/react @types/react-dom
    ```

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,6 +2,24 @@ import webpack from 'webpack';
 
 
 const config: webpack.Configuration = {
+  entry: './src/ts/index.tsx',
+  module: {
+    rules: [
+      // js, ts, tsx
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+           loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
 };
 
 
